5c557bf11c9dab7936aca4ba58033cc1ef129910
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-01-08 10:45:45 +0000

    0.0.1 +++ #i0001
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/t3foundation/trunk@82982 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,38 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'TYPO3 Foundation',
+	'description' => 'Responsive front-end framework foundation for TYPO3. See http://foundation.zurb.com/',
+	'category' => 'fe',
+	'shy' => 0,
+	'version' => '0.0.1',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 1,
+	'lockType' => '',
+	'author' => 'Dirk Wildt (Die Netzmacher)',
+	'author_email' => 'http://wildt.at.die-netzmacher.de',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '4.5.0-6.1.99',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'suggests' => array(
+	),
+);
+
+?>
---
+++ ext_tables.php
@@ -0,0 +1,153 @@
+<?php
+if( ! defined( 'TYPO3_MODE' ) )
+{
+  die( 'Access denied.' );
+}
+
+
+
+  ////////////////////////////////////////////////////////////////////////////
+  //
+  // INDEX
+
+  // Set TYPO3 version
+  // Configuration by the extension manager
+  //    Localization support
+  // Enables the Include Static Templates
+  // Add pagetree icons
+  // Methods for backend workflows
+
+
+
+  ////////////////////////////////////////////////////////////////////////////
+  //
+  // Set TYPO3 version
+
+  // Set TYPO3 version as integer (sample: 4.7.7 -> 4007007)
+list( $main, $sub, $bugfix ) = explode( '.', TYPO3_version );
+$version = ( ( int ) $main ) * 1000000;
+$version = $version + ( ( int ) $sub ) * 1000;
+$version = $version + ( ( int ) $bugfix ) * 1;
+$typo3Version = $version;
+  // Set TYPO3 version as integer (sample: 4.7.7 -> 4007007)
+
+if( $typo3Version < 3000000 ) 
+{
+  $prompt = '<h1>ERROR</h1>
+    <h2>Unproper TYPO3 version</h2>
+    <ul>
+      <li>
+        TYPO3 version is smaller than 3.0.0
+      </li>
+      <li>
+        constant TYPO3_version: ' . TYPO3_version . '
+      </li>
+      <li>
+        integer $this->typo3Version: ' . ( int ) $this->typo3Version . '
+      </li>
+    </ul>
+      ';
+  die ( $prompt );
+}
+  // Set TYPO3 version
+
+
+    
+
+  ////////////////////////////////////////////////////////////////////////////
+  //
+  // Configuration by the extension manager
+
+$confArr  = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['t3foundation']);
+
+  // Language for labels of static templates and page tsConfig
+$beLanguage = $confArr['beLanguage'];
+switch( $beLanguage )
+{
+  case( 'German'):
+    $beLanguage = 'de';
+    break;
+  default:
+    $beLanguage = 'default';
+}
+  // Language for labels of static templates and page tsConfig
+  // Configuration by the extension manager
+
+
+
+
+  ////////////////////////////////////////////////////////////////////////////
+  //
+  // Enables the Include Static Templates
+
+  // Case $beLanguage
+switch( true ) 
+{
+  case( $beLanguage == 'de' ):
+      // German
+    t3lib_extMgm::addStaticFile($_EXTKEY, 'static/5x/',  'Foundation 5x');
+    t3lib_extMgm::addStaticFile($_EXTKEY, 'static/4x/',  'Foundation 4x');
+//    switch( true )
+//    {
+//      case( $typo3Version < 4007000 ):
+//        t3lib_extMgm::addStaticFile($_EXTKEY,'static/typo3/4.6/', '+Flip it!: Basis fuer TYPO3 < 4.7 (einbinden!)');
+//        break;
+//      default:
+//        t3lib_extMgm::addStaticFile($_EXTKEY,'static/typo3/4.6/', '+Flip it!: Basis fuer TYPO3 < 4.7 (NICHT einbinden!)');
+//        break;
+//    }
+    break;
+  default:
+      // English
+    t3lib_extMgm::addStaticFile($_EXTKEY, 'static/5x/',  'Foundation 5x');
+    t3lib_extMgm::addStaticFile($_EXTKEY, 'static/4x/',  'Foundation 4x');
+//    switch( true )
+//    {
+//      case( $typo3Version < 4007000 ):
+//        t3lib_extMgm::addStaticFile($_EXTKEY,'static/typo3/4.6/', '+Flip it!: Basis for TYPO3 < 4.7 (obligate!)');
+//        break;
+//      default:
+//        t3lib_extMgm::addStaticFile($_EXTKEY,'static/typo3/4.6/', '+Flip it!: Basis for TYPO3 < 4.7 (don\'t use it!)');
+//        break;
+//    }
+    break;
+}
+  // Case $beLanguage
+  // Enables the Include Static Templates
+  
+
+
+  ////////////////////////////////////////////////////////////////////////////
+  //
+  // Add pagetree icons
+
+  // Case $beLanguage
+switch( true )
+{
+  case( $beLanguage == 'de' ):
+      // German
+    $TCA['pages']['columns']['module']['config']['items'][] =
+       array( 'Foundation', 't3foundatn', t3lib_extMgm::extRelPath( $_EXTKEY ).'ext_icon.gif' );
+    break;
+  default:
+      // English
+       array( 'Foundation', 't3foundatn', t3lib_extMgm::extRelPath( $_EXTKEY ).'ext_icon.gif' );
+}
+  // Case $beLanguage
+
+t3lib_SpriteManager::addTcaTypeIcon('pages', 'contains-t3foundatn', '../typo3conf/ext/t3foundation/ext_icon.gif');
+  // Add pagetree icons
+
+
+
+  ///////////////////////////////////////////////////////////
+  //
+  // Methods for backend workflows
+
+//require_once(t3lib_extMgm::extPath($_EXTKEY).'lib/flexform/class.tx_t3foundation_flexform.php');
+require_once(t3lib_extMgm::extPath($_EXTKEY).'lib/userfunc/class.tx_t3foundation_userfunc.php');
+  // Methods for backend workflows
+
+
+
+?>
