9de81b7f00d59855f6e72e65acb9bcd7de37a473
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-01-10 03:17:25 +0000

    0.0.3 +++ #54859
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/t3foundation/trunk@83106 735d13b6-9817-0410-8766-e36946ffe9aa

--- res/t3foundation/4.3.2/js/powermail2x.js
+++ res/t3foundation/4.3.2/js/powermail2x.js
@@ -15,7 +15,7 @@ jQuery( document ).ready( function( $ ) {
     $.each( invalid_fields, function( index, invalid_field ) {
       var fieldId = invalid_field.id;
       console.debug( invalid_field.id );      
-      tabId       = $( '#' + fieldId ).closest( "[data-section-title]" );
+      tabId       = $( '#' + fieldId ).closest( "[data-section-title]" ).attr( "id" );
       console.debug( tabId );      
       fieldsetId  = $( '#' + fieldId ).closest( "fieldset.powermail_fieldset" ).attr( "id" );
         // legend from current fieldset
