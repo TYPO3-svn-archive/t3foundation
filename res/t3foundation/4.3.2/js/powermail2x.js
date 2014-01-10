5f24f6caaf37304f2d5996951953868b39a560a6
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-01-10 00:17:34 +0000

    0.0.3 +++ #54859
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/t3foundation/trunk@83080 735d13b6-9817-0410-8766-e36946ffe9aa

--- res/t3foundation/4.3.2/js/powermail2x.js
+++ res/t3foundation/4.3.2/js/powermail2x.js
@@ -2,5 +2,5 @@
 // t3foundation/res/t3foundation/4.3.2/js/powermail2x.js
 
 $( document ).ready( function( ) {
-  $( "form.powermail_form" ).attr( "data-abide", "data-abide");
+  $( "form.powermail_form" ).attr( "data-abide", null );
 });
