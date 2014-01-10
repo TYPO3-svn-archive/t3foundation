71e89ce43ca5b1712a93ad0f512424cacae4ef3b
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-01-10 02:10:50 +0000

    0.0.3 +++ #54859
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/t3foundation/trunk@83091 735d13b6-9817-0410-8766-e36946ffe9aa

--- res/t3foundation/4.3.2/js/powermail2x.js
+++ res/t3foundation/4.3.2/js/powermail2x.js
@@ -2,17 +2,39 @@
 // t3foundation/res/t3foundation/4.3.2/js/powermail2x.js
 
 jQuery( document ).ready( function( $ ) {
+    // Add the attribute data-abide to the powermail form
   $( 'form.powermail_form' ).attr( 'data-abide', 'data-abide' );
+    // Add the attribute required to all powermail radio buttons
   $( '.powermail_radio' ).attr( 'required', 'required' );
+    // Reinit foundation abide
   $( document ).foundation( 'abide', 'events' );          // foundation 4.x
   // $('#your_form_id').foundation({bindings: 'events'}); // example foundation 5.x    
+    // 
   $('form.powermail_form').on( 'invalid', function ( ) {
     var invalid_fields = $( this ).find( '[data-invalid]' );
-    console.debug( invalid_fields );
+    var fieldId = invalid_field.attr( 'id' );
     $.each( invalid_fields, function( index, invalid_field ) {
-      console.debug( index );
-      console.debug( invalid_field );      
       console.debug( invalid_field.attr( 'id' ) );      
+      switch( invalid_field.attr( 'type' ) )
+      {
+        case( 'radio' ):
+        case( 'checkbox' ):
+          fieldWrapId = $( '#' + fieldId ).closest( "div.powermail_fieldwrap_check" ).attr( "id" );
+            // Id from closest fieldset
+          fieldsetId  = $( '#' + fieldWrapId ).closest( "fieldset" ).attr( "id" );
+            // legend from current fieldset
+          legend      = $( '#' + fieldsetId + " > legend" ).text( );
+          strAppend   = "<p>" + $( fieldsetId ).next( ).text( ) + ": <strong>[" + legend + "] " + $( fieldsetId ).next( ).text( ) + "</strong></p>";
+          break;
+        default:
+            // Id from closest fieldset
+          fieldsetId  = $( '#' + fieldId ).closest( "fieldset" ).attr( "id" );
+            // legend from current fieldset
+          legend      = $( '#' + fieldsetId + " > legend" ).text( );
+          strAppend   = "<p>" + $( fieldsetId ).next( ).text( ) + ": <strong>[" + legend + "] " + $( fieldsetId ).prev( ).text( ) + "</strong></p>";
+          break;
+      }
+      console.debug( strAppend );      
     })
   }).on( 'valid' , function ( ) {
     console.debug( 'valid!' );
