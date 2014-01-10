ccd3692485e98f68bed0cdf9b5bc90ed2804d99a
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-01-10 01:47:43 +0000

    0.0.3 +++ #54859
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/t3foundation/trunk@83089 735d13b6-9817-0410-8766-e36946ffe9aa

--- res/t3foundation/4.3.2/js/powermail2x.js
+++ res/t3foundation/4.3.2/js/powermail2x.js
@@ -2,14 +2,19 @@
 // t3foundation/res/t3foundation/4.3.2/js/powermail2x.js
 
 jQuery( document ).ready( function( $ ) {
-  $( "form.powermail_form" ).attr( "data-abide", "data-abide" );
-  $( ".powermail_radio" ).attr( "required", "required" );
-  $( document ).foundation( "abide", "events" );          // foundation 4.x
+  $( 'form.powermail_form' ).attr( 'data-abide', 'data-abide' );
+  $( '.powermail_radio' ).attr( 'required', 'required' );
+  $( document ).foundation( 'abide', 'events' );          // foundation 4.x
   // $('#your_form_id').foundation({bindings: 'events'}); // example foundation 5.x    
-  $('form.powermail_form').on('invalid', function () {
-    var invalid_fields = $(this).find('[data-invalid]');
-    console.debug(invalid_fields);
-  }).on('valid', function () {
-    console.debug('valid!');
+  $('form.powermail_form').on( 'invalid', function ( ) {
+    var invalid_fields = $( this ).find( '[data-invalid]' );
+    console.debug( invalid_fields );
+    $.each( invalid_fields, function( index, error, nochwas ) {
+      console.debug( index );
+      console.debug( error );      
+      console.debug( nochwas );      
+    })
+  }).on( 'valid' , function ( ) {
+    console.debug( 'valid!' );
   });  
 });
