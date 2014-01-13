1499e29493be12264a89a2d27f07e31e450339a3
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-01-13 23:34:53 +0000

    0.0.3 +++ #54859
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/t3foundation/trunk@83143 735d13b6-9817-0410-8766-e36946ffe9aa

--- res/t3foundation/4.3.2/js/powermail2x.js
+++ res/t3foundation/4.3.2/js/powermail2x.js
@@ -3,13 +3,15 @@
 
 jQuery( document ).ready( function( $ ) {
     // Add the attribute data-abide to the powermail form
+    // Use jQuery, because fluid doesn't accept data-abide attribute'
   $( 'form.powermail_form' ).attr( 'data-abide', 'data-abide' );
     // Add the attribute required to all powermail radio buttons
+    // Use jQuery, because fluid doesn't accept required attribute'
   $( '.powermail_radio' ).attr( 'required', 'required' );
     // Reinit foundation abide
   $( document ).foundation( 'abide', 'events' );          // foundation 4.x
   // $('#your_form_id').foundation({bindings: 'events'}); // example foundation 5.x    
-    // 
+    // Additional validating
   $('form.powermail_form').on( 'invalid', function ( ) {
     $( '[data-section-title]' ).removeClass( 'tab-error' );
     var invalid_fields = $( this ).find( '[data-invalid]' );
@@ -19,6 +21,7 @@ jQuery( document ).ready( function( $ ) {
       tabId       = $( '#' + fieldId ).closest( 'p[data-section-title]' ).attr( 'id' );
       sectionId   = $( '#' + fieldId ).closest( 'section' ).attr( 'id' );
       tabId       = $( '#' + sectionId + ' > [data-section-title]').attr( 'id' );
+        // tabs with invalid fields get the tab-error class
       $( '#' + tabId ).addClass( 'tab-error' );
       console.debug( tabId );      
       fieldsetId  = $( '#' + fieldId ).closest( 'fieldset.powermail_fieldset' ).attr( 'id' );
