a7735a8a37f69ef9b9c6895c959e89476f2e36ef
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-11-22 21:58:33 +0000

    1.0.0 +++ #i0011, #i0012
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/t3foundation/trunk@86956 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/Foundation/5x/page/setup.ts
+++ Configuration/TypoScript/Foundation/5x/page/setup.ts
@@ -1,12 +1,12 @@
 page {
   includeCSS {
-    t3foundation5x = EXT:t3foundation/Resources/Public/JavaScript/Libs/Foundation-5.0.2/css/foundation.min.css
+    t3foundation5x = EXT:t3foundation/Resources/Public/JavaScript/Libs/Foundation-5.4.7/css/foundation.min.css
   }
   includeJSlibs {
-    t3foundation5xModernizr = EXT:t3foundation/Resources/Public/JavaScript/Libs/Foundation-5.0.2/js/modernizr.js
+    t3foundation5xModernizr = EXT:t3foundation/Resources/Public/JavaScript/Libs/Foundation-5.4.7/js/modernizr.js
   }
   includeJSFooterlibs {
-    t3foundation5x = EXT:t3foundation/Resources/Public/JavaScript/Libs/Foundation-5.0.2/foundation.min.js
+    t3foundation5x = EXT:t3foundation/Resources/Public/JavaScript/Libs/Foundation-5.4.7/foundation.min.js
   }
   includeJSFooter {
     t3foundation5x = EXT:t3foundation/Resources/Public/JavaScript/Libs/foundation.js
